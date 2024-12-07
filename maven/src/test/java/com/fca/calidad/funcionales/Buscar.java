f6654dca07b670d2c97e5ee9f637931ada2bb3a9
YAJAI <YAJAI@192.168.0.6>
2024-12-07 17:05:03 -0600

    m

--- maven/src/test/java/com/fca/calidad/funcionales/Buscar.java
+++ maven/src/test/java/com/fca/calidad/funcionales/Buscar.java
@@ -1,78 +1,100 @@
 package com.fca.calidad.funcionales;
-
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.apache.commons.io.FileUtils;
+import java.io.File;
 import java.time.Duration;
 
-public class Buscar {
-    private WebDriver driver;
-    private String baseUrl;
-    private StringBuffer verificationErrors = new StringBuffer();
-    JavascriptExecutor js;
+public class Buscar{
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+	  WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+    js = (JavascriptExecutor) driver;
+  }
+//editar
+  @Test
+  public void testEd2() throws Exception {
+    driver.get("https://mern-crud-mpfr.onrender.com/");
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("nuevo");
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys("nuevo@gmail.com");
+    driver.findElement(By.name("age")).click();
+    driver.findElement(By.name("age")).clear();
+    driver.findElement(By.name("age")).sendKeys("22");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[1]/following::div[2]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+    driver.findElement(By.xpath("//i")).click();
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button")).click();
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("Nuevocambio");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+    driver.findElement(By.xpath("//i")).click();
+  }
 
-    @Before
-    public void setUp() throws Exception {
-        // Configuración del WebDriver
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        baseUrl = "https://mern-crud-mpfr.onrender.com/";
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        js = (JavascriptExecutor) driver;
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
     }
+  }
 
-    @Test
-    public void testBuscarUsuarioPrueba1() throws Exception {
-        // Navegar a la página base
-        driver.get(baseUrl);
-
-        // Agregar usuario "prueba1"
-        driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
-        driver.findElement(By.name("name")).sendKeys("prueba1");
-        driver.findElement(By.name("email")).sendKeys("prueba1@gmail.com");
-        driver.findElement(By.name("age")).sendKeys("22");
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[2]/following::div[1]")).click();
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
-
-        // Pausar brevemente para asegurarse de que los datos se cargan (solo para pruebas)
-        Thread.sleep(2000);
-
-        // Buscar en la columna "Name" de la tabla
-        boolean usuarioEncontrado = false;
-
-        try {
-            WebElement tablaUsuarios = driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody"));
-            for (WebElement fila : tablaUsuarios.findElements(By.tagName("tr"))) {
-                WebElement columnaName = fila.findElement(By.xpath("./td[1]")); // Cambia el índice de la columna según el orden de Name
-                String textoColumna = columnaName.getText();
-                if (textoColumna.equalsIgnoreCase("prueba1")) {
-                    usuarioEncontrado = true;
-                    System.out.println("Usuario 'prueba1' encontrado en la columna Name.");
-                    break;
-                }
-            }
-        } catch (NoSuchElementException e) {
-            System.out.println("No se encontró la tabla o la columna Name.");
-        }
-
-        // Verificar si el usuario fue encontrado
-        if (!usuarioEncontrado) {
-            System.out.println("Usuario 'prueba1' NO fue encontrado en la columna Name.");
-        }
-
-        // Afirmar que el usuario está en la tabla
-        assertTrue("El usuario 'prueba1' no fue encontrado en la columna Name.", usuarioEncontrado);
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
     }
+  }
 
-    @After
-    public void tearDown() throws Exception {
-        driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
     }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
 }
